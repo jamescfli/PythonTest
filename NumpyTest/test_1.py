a6ebc46e94d2b2b5286d033dec1d2aff27561af6
James Li <jamescfli@yahoo.com>
2017-03-08 23:52:46 +0800

    20 numpy tests from https://github.com/rougier/numpy-100/blob/master/100%20Numpy%20exercises.md

--- NumpyTest/test_1.py
+++ NumpyTest/test_1.py
@@ -6,3 +6,39 @@ np.show_config()        # include openblas atlas etc.
 
 foo = np.zeros((10,10))     # float64, 8 bytes
 print("%d bytes" % (foo.size * foo.itemsize))   # 800
+
+np.info(np.add)
+
+np.arange(10, 50)
+
+nz = np.nonzero([1,2,0,0,4,0])
+print nz
+
+np.eye(3,4)
+# array([[ 1.,  0.,  0.,  0.],
+#        [ 0.,  1.,  0.,  0.],
+#        [ 0.,  0.,  1.,  0.]])
+
+foo = np.random.random((3, 3))
+foo.min()
+foo.max()
+
+foo = np.ones((5,5))
+foo = np.pad(foo, pad_width=1, mode='constant', constant_values=0)
+print(foo)
+
+print(0.3 == 3 * 0.1)   # False
+# 3*0.1 = 0.30000000000000004
+
+np.diag(1+np.arange(4),k=-1)
+# where The default is 0. Use
+#   k>0 for diagonals above the main diagonal,
+#   and k<0 for diagonals below the main diagonal.
+
+
+foo = np.zeros((8,8),dtype=int)
+foo[1::2,::2] = 1
+foo[::2,1::2] = 1
+print(foo)
+
+print(np.unravel_index(100,(6,7,8)))
