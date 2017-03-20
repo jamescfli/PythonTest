1f3ca814413826750d6426adc2a681f022a5e250
James Li <jamescfli@yahoo.com>
2017-03-20 17:12:50 +0800

    add video file write from webm to mp4

--- OpenCVTest/ReadWebMFile/read_n_show.py
+++ OpenCVTest/ReadWebMFile/read_n_show.py
@@ -2,29 +2,30 @@ import cv2
 
 
 # video_filepath = './video_clips/b.mp4'      # no problem for mp4
-# video_filepath = './video_clips/b.webm'
-video_filepath = './video_clips/test.webm'
+video_filepath = './video_clips/b.webm'
+# video_filepath = './video_clips/test.webm'
 # .. Issue: VIDEOIO(cvCreateFileCapture_AVFoundation (filename)): raised unknown C++ exception!
 
 print("loading {}".format(video_filepath))
-# cap = cv2.VideoCapture(video_filepath)
-cap = cv2.VideoCapture(video_filepath, cv2.CAP_FFMPEG)  # after brew install ffmpeg
+cap = cv2.VideoCapture(video_filepath)
+# cap = cv2.VideoCapture(video_filepath, cv2.CAP_FFMPEG)  # after brew install ffmpeg
 print("capture finished")
 
-# output_shape = (480, 960)
-# # const char* filename, int fourcc, double fps, CvSize frame_size, int is_color=1 (gray or color)
-# out = cv2.VideoWriter('../videos/output.mp4', -1, 60.0, output_shape[::-1])
-# print('finish init video writer')
+output_shape = (480, 960)
+# const char* filename, int fourcc, double fps, CvSize frame_size, int is_color=1 (gray or color)
+out = cv2.VideoWriter('./video_out/output.mp4', -1, 60.0, output_shape[::-1])
+print('finish init video writer')
 
 frame_counter = 0
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret:
         frame_counter += 1
+        out.write(frame)
     else:
         break
 print frame_counter     # 2473 frames for b.mp4
 
 cap.release()
-# out.release()
+out.release()
 cv2.destroyAllWindows()
