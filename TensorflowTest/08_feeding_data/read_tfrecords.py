7d8d1f0074653561c377880248bcf04ce5855dfa
James Li <jamescfli@yahoo.com>
2017-06-05 16:08:42 +0800

    added data feeding examples for tensorflow

---
+++ TensorflowTest/08_feeding_data/read_tfrecords.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os.path
+import time
+
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import mnist
+import cfgs.tf_config_read as cfg
+
+# Constants used for dealing with the files, matches convert_to_records.
+TRAIN_FILE = 'train.tfrecords'
+VALIDATION_FILE = 'valid.tfrecords'
+
+
+def read_and_decode(filename_queue):
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'image_raw': tf.FixedLenFeature([], tf.string),
+                                           'label': tf.FixedLenFeature([], tf.int64),
+                                       })   # output Tensor
+    image = tf.decode_raw(features['image_raw'], tf.uint8)
+    # .. others: tf.image.decode_jpeg, tf.image.decode_png, tf.image.decode_image
+    image.set_shape([mnist.IMAGE_PIXELS])   # image.shape = (784,)
+    # convert to [-0.5, +0.5]
+    image = tf.cast(image, tf.float32) * (1./255) - 0.5
+    label = tf.cast(features['label'], tf.int32)
+    return image, label
+
+
+def inputs(train, batch_size, nb_epochs):
+    # Note that an tf.train.QueueRunner is added to the graph, which
+    # must be run using e.g. tf.train.start_queue_runners().
+    if not nb_epochs: nb_epochs = None
+    filename = os.path.join(cfg.FLAGS.train_dir, TRAIN_FILE if train else VALIDATION_FILE)
+    # .. could be with multiple files
+    with tf.name_scope('input'):
+        filename_queue = tf.train.string_input_producer([filename], num_epochs=nb_epochs)   # output a FIFOQuue
+        image, label = read_and_decode(filename_queue)  # output two tensors
+        # shuffle the examples and collect them into batch_size batches
+        # internally, uses a RandomShuffleQueue
+        # we run this in two threads to avoid being a bottleneck
+        images, sparse_labels = tf.train.shuffle_batch(
+            [image, label],
+            batch_size=batch_size,
+            num_threads=2,
+            capacity=1000 + 3 * batch_size,     # 3 = 2 threads + 1 margin
+            # ensures a minimum amount of shuffling of examples
+            min_after_dequeue=1000
+        )
+        # min_after_dequeue:
+        #     Minimum number elements/samples in the queue after a dequeue,
+        #     used to ensure a level of mixing (shuffling) of elements.
+        #     in mnist example validation set has 5000 samples, so min_after_dequeue = 1000
+        # capacity: An integer. The maximum number of elements in the queue.
+        # Steps:
+        #   1) determine min_after_dequeue first, big -> more fair shuffle but slow start and large memory consumption
+        #   2) decide capacity according to min_after_dequeue + (num_threads + a small safety margin) * batch_size
+        return images, sparse_labels    # output tensors
+
+
+def run_training():
+    with tf.Graph().as_default():
+        images, labels = inputs(train=True, batch_size=cfg.FLAGS.batch_size, nb_epochs=cfg.FLAGS.nb_epochs)
+        logits = mnist.inference(images, cfg.FLAGS.hidden1, cfg.FLAGS.hidden2)
+        loss = mnist.loss(logits, labels)
+        train_op = mnist.training(loss, cfg.FLAGS.learning_rate)
+        init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+
+        sess = tf.Session()
+        sess.run(init_op)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)  # Thread 6~10, 5 on MBP
+
+        try:
+            step = 0
+            while not coord.should_stop():
+                start_time = time.time()
+                _, loss_value = sess.run([train_op, loss])
+                duration = time.time() - start_time
+                if step % 100 == 0:
+                    print('Step {}: loss = {:.02f} ({:.03f} sec)'.format(step, loss_value, duration))
+                step += 1
+        except tf.errors.OutOfRangeError:
+            print('Done training for {} epochs, {} steps'.format(cfg.FLAGS.nb_epochs, step))
+        finally:
+            coord.request_stop()
+
+        coord.join(threads)
+        sess.close()
+
+
+def main(_):
+    run_training()
+
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ TensorflowTest/08_feeding_data/write_tfrecords.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Converts MNIST data to TFRecords
+
+# absolute_import does not care about whether something is part of the standard library,
+# and import string will not always give you the standard-library module with absolute imports on.
+# from __future__ import absolute_import means that if you import string,
+# Python will always look for a top-level string module, rather than current_package.string
+from __future__ import absolute_import
+from __future__ import division     # print(1/2) = 0.5
+from __future__ import print_function
+
+import os
+import sys
+# sys.path.append('.')
+
+import tensorflow as tf
+
+from tensorflow.contrib.learn.python.learn.datasets import mnist
+import cfgs.tf_config_write as cfg
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def convert_to_tfrecords(data_set, name):
+    images = data_set.images    # shape (nb_samples, 28, 28, 1), dtype np.uint8
+    labels = data_set.labels    # shape (nb_samples), dtype np.uint8, range 0~9
+    num_examples = data_set.num_examples
+
+    if images.shape[0] != num_examples:
+        raise ValueError('Images size {} does not match label size {}.'.format(images.shape[0], num_examples))
+    rows = images.shape[1]
+    cols = images.shape[2]
+    depth = images.shape[3]
+
+    filename = os.path.join(cfg.FLAGS.directory, name + '.tfrecords')
+    print('Writing {}'.format(filename))
+    writer = tf.python_io.TFRecordWriter(filename)
+    for index in range(num_examples):
+        image_raw = images[index].tostring()
+        # class, initialized with features
+        # Magic attribute generated for "features" proto field.
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'height': _int64_feature(rows),
+            'width': _int64_feature(cols),
+            'depth': _int64_feature(depth),
+            'label': _int64_feature(int(labels[index])),
+            'image_raw': _bytes_feature(image_raw)
+        }))
+        # features -> feature -> image_raw -> byteList -> value -> 0x/**
+        #                     -> depth    -> int64List -> value -> 1
+        #                     -> label
+        #                     -> width
+        #                     -> height
+        writer.write(example.SerializeToString())
+    writer.close()
+
+
+def main(_):        # _ : unused_argv
+    # class type: Dataset
+    data_sets = mnist.read_data_sets(cfg.FLAGS.directory,
+                                     dtype=tf.uint8,
+                                     reshape=False,
+                                     validation_size=cfg.FLAGS.validation_size)
+    convert_to_tfrecords(data_sets.train, 'train')
+    convert_to_tfrecords(data_sets.validation, 'valid')
+    convert_to_tfrecords(data_sets.test, 'test')
+
+
+if __name__ == '__main__':
+    tf.app.run()
