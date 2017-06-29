168c859c192598c5778f7f399828e1c6e5fee3a2
James Li <jamescfli@yahoo.com>
2017-06-29 17:25:48 +0800

    to test SeLU activation to speed up training

---
+++ TensorflowTest/11_selu_test/selu.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function
+
+from tensorflow.python.framework import ops
+import tensorflow as tf
+
+
+def selu(x):
+    with ops.name_scope('elu') as scope:
+        alpha = 1.6732632423543772848170429916717
+        scale = 1.0507009873554804934193349852946
+        return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x))
